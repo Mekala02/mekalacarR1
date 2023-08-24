1367ca5d8fce4960c2e6b07ca27943e23f6636df
mekala02 <ernkla@gmail.com>
2023-08-24 14:16:24 +0300

    add masters url

--- docker/scripts/ros_entrypoint.sh
+++ docker/scripts/ros_entrypoint.sh
@@ -3,4 +3,12 @@ set -e
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
+
+IP=$(hostname -I)
+array=($IP)
+IP=${array[0]}
+
+export ROS_MASTER_URI=http://$IP:11311
+export ROS_IP=${array[0]}
+
 exec "$@"
